2a6bf10565f0a36241c3e6a9aa1a8aab0bd04180
sevval14 <sevvalozekinci85@gmail.com>
2024-06-21 23:35:54 +0300

    added some deafult value for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    
+    extend: {
+      fontFamily:{
+        roboto:["Roboto", "sans-serif"]
+      } ,
+      colors:{
+        "yellow_dark":"#FBBF24",
+        "background" :"#FFFBEB",
+        "text_color" :"#78350F",
+        "sneakers":"#FBBF24",
+        "card_color":"#0F172A"
+
+    }
+    
+
+    },
   },
   plugins: [],
 }
