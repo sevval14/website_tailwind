156ef648e92322e0846ee0c47ac93df69e2beea8
sevval14 <sevvalozekinci85@gmail.com>
2024-06-22 21:18:08 +0300

    added some shortcuts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    
     extend: {
-      fontFamily:{
-        roboto:["Roboto", "sans-serif"]
-      } ,
-      colors:{
-        "yellow_dark":"#FBBF24",
-        "background" :"#FFFBEB",
-        "text_color" :"#78350F",
-        "sneakers":"#FBBF24",
-        "card_color":"#0F172A"
-
-    }
-    
-
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"]
+      },
+      colors: {
+        yellow_dark: "#FBBF24",
+        background: "#FFFBEB",
+        text_color: "#78350F",
+        sneakers: "#FBBF24",
+        card_color: "#0F172A",
+        collected :"#78350F",
+        collected_text:"#FFFBEB"
+      },
+      fontSize: {
+        xs: '12px', // Extra small text
+        sm: '14px', // Small text
+        base: '16px', // Base text
+        lg: '18px', // Large text
+        xl: '20px', // Extra large text
+        '2xl': '24px', // 2X large text
+        '3xl': '30px', // 3X large text
+        '4xl': '36px', // 4X large text
+        '5xl': '48px', // 5X large text
+        '6xl': '60px', // 6X large text
+      }
     },
   },
   plugins: [],
 }
-
